bf8f99cf420148e77f9f81517daa16cbf2198645
Shian Su <su.s@wehi.edu.au>
2016-08-29 01:09:56 +0000

    Version update
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Glimma@120540 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/utilities.R
+++ R/utilities.R
@@ -86,34 +86,21 @@ pathMaker <- function(path) {
     }
 }
 
-#' String to hex colour converter
-#'
-#' Function to convert colour strings into hex codes
-#'
-#' @param x the string colour value(s) to be converted to hex values.
-#'
-#' @return hex codes for colours
-#'
-#' @importFrom grDevices col2rgb
+# String to hex colour converter
+
 
 CharToHexCol <- function(x) {
+    requireNamespace("grDevices")
     temp <- as.character(as.hexmode(col2rgb(x, alpha=FALSE)))
     out <- apply(temp, 2, function(x) {paste0("#", paste0(x, collapse=""))})
     sapply(out, function(x) { ifelse(x=="#000", "#000000", x) })
 }
 
-#' Numeric to hex colour converter
-#'
-#' Functions to convert numbers into corresponding hex codes for colours
-#'
-#' @param x the colour value(s) to be converted to hex values.
-#'
-#' @return hex codes for colours
-#'
-#' @importFrom grDevices palette col2rgb
+# Numeric to hex colour converter
 
 NumToHexCol <- function(x) {
-    col <- palette()[as.integer(col)]
+    requireNamespace("grDevices")
+    x <- palette()[as.integer(x)]
     temp <- as.character(as.hexmode(col2rgb(x, alpha=FALSE)))
     out <- apply(temp, 2, function(x) {paste0("#", paste0(x, collapse=""))})
     sapply(out, function(x) { ifelse(x=="#000", "#000000", x) })
@@ -124,7 +111,22 @@ is.hex <- function(x) {
     (grepl("#[[:xdigit:]]{6}", x) | grepl("#[[:xdigit:]]{8}", x))
 }
 
-# Function to convert colours or numbers to hex colour values
+#' Numeric to hex colour converter
+#'
+#' Functions to convert numbers into corresponding hex codes for colours
+#'
+#' @param x the colour value(s) to be converted to hex values.
+#'
+#' @return hex codes for colours
+#' 
+#' @examples
+#' as.hexcol(c(1, 2, 3))
+#' as.hexcol(c("red", "black", "green"))
+#'
+#' @importFrom grDevices palette col2rgb
+#' 
+#' @export
+
 as.hexcol <- function(x) {
     if (is.character(x)) {
         if (all(is.hex(x))) {
