1af88e31fa1f7e7b6b7cd4c3e38d72a0d786e317
hasaru-k <35970468+hasaru-k@users.noreply.github.com>
2025-02-08 15:04:12 +1100

    github sync

--- inst/htmlwidgets/lib/libXY/expressionSpec.js
+++ inst/htmlwidgets/lib/libXY/expressionSpec.js
@@ -1,4 +1,12 @@
-function createExpressionSpec(width, height, expColumns, sampleColours, samples)
+function createExpressionSpec(
+    width, 
+    height, 
+    expColumns, 
+    sampleColours, 
+    samples, 
+    minExtentInputClass, 
+    maxExtentInputClass,
+)
 {
 
     let colourscheme_signal = 
@@ -44,7 +52,7 @@ function createExpressionSpec(width, height, expColumns, sampleColours, samples)
                         "value": null,
                         "bind": { 
                                   "input": "number",
-                                  "class": "min_extent_input",
+                                  "class": minExtentInputClass,
                                   "name": "Y min",
                                 },
                     },
@@ -53,19 +61,25 @@ function createExpressionSpec(width, height, expColumns, sampleColours, samples)
                         "value": null,
                         "bind": { 
                                   "input": "number",
-                                  "class": "max_extent_input",
+                                  "class": maxExtentInputClass,
                                   "name": "Y max",
                                 },
                     },
                     {
                         "name": "min_y",
-                        // min Y value must be less than the range minimum
                         "update": " (min_y_input > extent[0]) ? null : min_y_input"
                     },
                     {
                         "name": "max_y",
-                        // max Y value must be greater than the range maximum
                         "update": " (max_y_input < extent[1]) ? null : max_y_input"
+                    },
+                    {
+                        "name": "jitter", 
+                        "value": 0,
+                        "bind": { 
+                                "input": "range",
+                                "name": "jitter",
+                                },
                     },
                     sampleColours == -1 ? colourscheme_signal : samplecols_signal
                 ],
@@ -134,8 +148,11 @@ function createExpressionSpec(width, height, expColumns, sampleColours, samples)
                         "stroke": {"value": "#575757"},
                         "tooltip": tooltip
                     }
-                }
+                },
+                "transform": [
+                    {"type": "formula", "as": "x0", "expr": "datum.x"},
+                    {"type": "formula", "as": "x", "expr": "datum.x0 + (random()-0.5)*jitter"},
+                ],
             }]
     };
-
 }
